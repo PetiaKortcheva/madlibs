b12ece686764a1c1e22c13a3e2c6e936f526141f
Ryan Ashby <rjapsu160@gmail.com>
2021-10-25 15:19:28 -0700

    add loops for madlib page.
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    console.log(person1Input);
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    let inputs = ["#person1", "#person2", "#animal", "#exclamation", "#verb" , "#noun"]
+    let spans = [".person1", ".person2", ".animal", ".exclamation", ".verb" , ".noun"]
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
+    inputs.forEach(function(input, index) {
+        $(spans[index]).text($(input).val())
+    });
     $("#story").show();
   });
 });
