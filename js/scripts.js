0f9d628a2257061c66e7e19ab6f6b3c1a70ef137
Ryan Ashby <rjapsu160@gmail.com>
2022-01-11 13:54:11 -0800

    follow lesson for input vals and text into spans
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,34 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    let inputs = ["#person1", "#person2", "#animal", "#exclamation", "#verb" , "#noun"]
-    let spans = [".person1", ".person2", ".animal", ".exclamation", ".verb" , ".noun"]
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput= $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
 
-    inputs.forEach(function(input, index) {
-        $(spans[index]).text($(input).val())
-    });
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
     $("#story").show();
   });
 });
+
+
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     event.preventDefault();
+//     let inputs = ["#person1", "#person2", "#animal", "#exclamation", "#verb" , "#noun"]
+//     let spans = [".person1", ".person2", ".animal", ".exclamation", ".verb" , ".noun"]
+
+//     inputs.forEach(function(input, index) {
+//         $(spans[index]).text($(input).val())
+//     });
+//     $("#story").show();
+//   });
+// });
